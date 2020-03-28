ee99fbc49062a74c49790ecb138088869cdc7c5d
Jose Olcese <jolcese@amazon.com>
2020-03-27 18:34:38 -0700

    Get better population information

--- helpers/getCountryData.js
+++ helpers/getCountryData.js
@@ -1,48 +1,76 @@
 
-const csv = require('csv-parser');
+// const csv = require('csv-parser');
 const fs = require('fs');
 const https = require('https');
+const request = require('request');
 
-if (fs.existsSync('countries-population.csv')) fs.unlinkSync('countries-population.csv');
+if (fs.existsSync('../data/countries-population.csv')) fs.unlinkSync('../data/countries-population.csv');
 
-fs.createReadStream('countries.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    // console.log(row);
-
-
+async function process() { 
+  
+  const req = await downloadPage('https://corona.lmao.ninja/v2/historical');
+  
+  // console.log(req);
+  // for(let country of JSON.parse(req).) { 
+  JSON.parse(req).forEach( country => {
+    var requestCountry = country.country;
+    var requestProvince = country.province;
     
-    https.get('https://restcountries.eu/rest/v2/name/' + row.country, (resp) => {
-      let data = '';
+    if ((country.country == 'france' || country.country == 'uk' || country.country == 'denmark' || country.country == 'netherlands') && country.province) {
+      requestCountry = country.province;
+      requestProvince = null;
+    }
     
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
+    if (requestProvince) {
+      console.log(requestCountry + ' ' + requestProvince);
+    }
+    // console.log(requestCountry);
+    https.get('https://restcountries.eu/rest/v2/name/' + requestCountry, (resp) => {
+    let data = '';
     
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        // console.log(row);
-        // console.log(JSON.stringify(resp.getHeaders()));
-
-        if (Array.isArray(JSON.parse(data)) ) {
-          fs.writeFileSync(JSON.parse(data)[0].name + '.json', data);
-
-          var pop = JSON.parse(data)[0].population;
-          if (row.country == 'india') pop = 1339000000;
-
-          fs.appendFileSync('countries-population.csv', row.country + ',' + JSON.parse(data)[0].population + '\n');
-
-        } else {
-          console.log(row);
-        }
-      });
-    
-
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
     });
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
+    
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      // console.log(country);
+      // console.log(JSON.stringify(resp.getHeaders()));
+      
+      if (Array.isArray(JSON.parse(data)) ) {
+        //fs.writeFileSync(JSON.parse(data)[0].name + '.json', data);
+        var pop = JSON.parse(data)[0].population;
+        if (requestCountry == 'india') pop = 1339000000;
+        
+        // if (country.province) console.log('Country: ' + country.country + ' - province: ' + country.province + ' - Pop: ' + pop);
+        
+        fs.appendFileSync('../data/countries-population.csv', requestCountry + ',' + pop + '\n');
+        
+      } else {
+        console.log('**** NO **** -> ' + requestCountry);
+      }
+    });
+    
+    
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
   });
+});
+}
+
+process();
+
+// wrap a request in an promise
+function downloadPage(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode != 200) {
+        reject('Invalid status code <' + response.statusCode + '>');
+      }
+      resolve(body);
+    });
+  });
+}
+
